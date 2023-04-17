270128908b42b045b37274ff5cbbb39e41ab4432
m0000amir <mukhtarov.amir.a@gmail.com>
2023-04-17 17:10:07 +0300

    added coderun

---
+++ yandex/coderun/frontend/carnations.js
@@ -0,0 +1,33 @@
+// Для чтения входных данных в Node.js необходимо использовать
+// модуль readline, который работает с потоком ввода-вывода
+// (stdin/stdout) и позволяет читать строки.
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Данные во входном потоке могут состоять из нескольких строк.
+// Чтобы прочитать их, можно использовать метод rl.on(),
+// который вызывается каждый раз при появлении новой строки
+// в потоке ввода.
+// Чтобы вывести результат в поток вывода (stdout),
+// можно использовать метод console.log().
+// Пример:
+// console.log('Результат:', result);
+
+// Пример решения задачи "Вычислите сумму A+B":
+rl.on('line', line2 => {
+  console.log(line2);
+  rl.close();
+});
+
+rl.on('line', line1 => {
+  const n = line1.split('')
+  const coordinates = line1.split(' ').map(Number);
+
+  console.log('hello!');
+  console.log(coordinates);
+  rl.close();
+});
