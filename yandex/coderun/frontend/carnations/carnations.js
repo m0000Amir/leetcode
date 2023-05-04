8abb7b17b533da4c4377b4dea1ab7ae9804416df
m0000amir <mukhtarov.amir.a@gmail.com>
2023-05-04 16:18:41 +0300

    carnation problem done it

--- yandex/coderun/frontend/carnations/carnations.js
+++ yandex/coderun/frontend/carnations/carnations.js
@@ -8,26 +8,36 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// Данные во входном потоке могут состоять из нескольких строк.
-// Чтобы прочитать их, можно использовать метод rl.on(),
-// который вызывается каждый раз при появлении новой строки
-// в потоке ввода.
-// Чтобы вывести результат в поток вывода (stdout),
-// можно использовать метод console.log().
-// Пример:
-// console.log('Результат:', result);
+let counter = 0;
+let n;
+let points = [];
+let dp = [];
 
-// Пример решения задачи "Вычислите сумму A+B":
-rl.on('line', line2 => {
-  console.log(line2);
-  rl.close();
-});
+const getMinLength = () => {
+  if (points.length === 1) {
+    console.log(points[0]);
+  } else {
+    points.sort((a, b) => a - b);
+    dp[1] = points[1] - points[0];
+    dp[2] = points[2] - points[0];
+    for (let i = 3; i < n; i++) {
+      dp[i] = Math.min(dp[i-1], dp[i-2]) + points[i] - points[i-1];
+    }
+    console.log(dp[n-1]);
+  }
+}
 
-rl.on('line', line1 => {
-  const n = line1.split('')
-  const coordinates = line1.split(' ').map(Number);
-
-  console.log('hello!');
-  console.log(coordinates);
-  rl.close();
+rl.on('line', line => {
+  if (counter === 0) {
+    n = Number(line);
+    counter += 1;
+  } else {
+    points = line.split(' ').map(Number);
+    points = points.slice(0, n);
+    counter += 1;
+  }
+  if (counter >= 2) {
+    rl.close();
+    getMinLength();
+  }
 });
